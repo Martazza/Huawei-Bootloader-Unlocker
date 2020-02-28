4a0b4ef3174acd42c436479c46b222673203603a
Martazza <Martazza@users.noreply.github.com>
2020-02-28 22:09:42 +0100

    Quicker restart

--- huawei_bootloader_unlocker.c
+++ huawei_bootloader_unlocker.c
@@ -3,6 +3,8 @@
     This is intended to be the fastest ever created bootloader unlocker
 */
 #include <signal.h>
+#include <stdlib.h>
+
 #define LEN 37
 
 long long base_start = 1000000000000000;
@@ -12,13 +14,17 @@ void resumer(){
    return 1;
 }
 
-int main() {
+int main( int argc, char **argv) {
+	if ( argc > 1 ) {
+		char *base = argv[1];
+		base_start = atoll( base );
+	}
     signal(SIGINT, resumer);
     char fou[LEN] = {'f', 'a', 's', 't', 'b', 'o', 'o', 't', ' ',
     'o', 'e', 'm', ' ', 'u', 'n', 'l', 'o', 'c', 'k', ' '};
     char TOTAL[LEN];
 
-    while (sprintf(TOTAL, "%s%lld", fou,base_start++) && system(TOTAL));
+    while (sprintf( TOTAL, "%s%lld", fou, base_start++) && system( TOTAL ));
 
     printf("Your unlock code is: ", base_start);
 
